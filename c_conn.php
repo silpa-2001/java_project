e4d8ca44f9dbebc44d376c7ac7006053109d653b
silpa-2001 <161181980+silpa-2001@users.noreply.github.com>
2024-03-07 20:59:17 +0530

    Add files via upload

---
+++ c_conn.php
@@ -0,0 +1,8 @@
+<?php
+    $username= "root";
+    $password="";
+    $server_name="localhost";
+    $db_name="electrostore";
+
+    $con =new mysqli($server_name, $username, $password, $db_name);
+?>
